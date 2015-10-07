2f52aeaef1d52065961ab39136b50ea2cb10f4d0
warakorn.p <warakorn.p@TONG_IT.srivichai3.local>
2015-10-07 12:54:15 +0700

    - Session Rolename -> Pass - Summary On Site  -> Pass - ListDual -> No Save Loop

--- frontend/controllers/SummaryOnSiteController.php
+++ frontend/controllers/SummaryOnSiteController.php
@@ -11,6 +11,7 @@ use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use yii\helpers\Json;
+
 /**
  * SummaryOnSiteController implements the CRUD actions for SummaryOnSite model.
  */
@@ -63,29 +64,29 @@ class SummaryOnSiteController extends Controller {
         $software = new Software();
         $get_software_id = Yii::$app->getRequest()->get('s_id');
         $att_computer = Json::decode($model->computer_id);
-        
-        if ($model->load(Yii::$app->request->post()) && isset($_POST['SummaryOnSite'])) {
-            $model->attributes = $_POST['SummaryOnSite'];
-            //print_r($model->software_id = $get_software_id);
-            echo "on loop"."<br>";
-            for ($i = 0; $i < count($att_computer); $i++):                
-                $model->summary_id = NULL;
-                //$model->isNewRecord = true;
-                $model->software_id = $get_software_id;
-                $model->computer_id = $_POST['SummaryOnSite']['computer_id'];
-                $model->save();
-                echo "in loop"."<br>";
-                echo $i;      
-                
-                if ($model->save()) {
-                    echo "if save";                   
-                    Yii::$app->session->setFlash('green-' . $i, '<i class="fa fa-info-circle"></i> <b>Fees Category:</b> ' . $_POST['SummaryOnSite']['computer_id'] . ' for <b>Batch: </b>' . ' is created successfully');
-                } else {
-                    echo "else save";
-                    Yii::$app->session->setFlash('red-' . $i, '<i class="fa fa-warning"></i> The combination of <b>Fees Category:</b> ' . $_POST['SummaryOnSite']['computer_id'] . ' has already been taken.');
-                }
-            endfor;
-            // exit(); 
+
+        if ($model->load(Yii::$app->request->post())) {
+//            $model->attributes = $_POST['SummaryOnSite'];
+//            print_r($model->software_id = $get_software_id);
+//            echo "on loop"."<br>";
+              $model->computer_id = $att_computer;
+//            $model->save(false);
+//            for ($i = 0; $i < count($att_computer); $i++):                
+//            $model->summary_id = null;
+//            $model->isNewRecord = true;
+              $model->software_id = $get_software_id;
+//            $model->computer_id = $_POST['SummaryOnSite']['computer_id'][$att_computer];
+//            $model->errors;
+              $model->save();
+            //exit(); 
+//            if ($model->save(false)) {
+//                echo "if save";
+//                Yii::$app->session->setFlash('green-', '<i class="fa fa-info-circle"></i> <b>Fees Category:</b> ' . $_POST['SummaryOnSite']['computer_id'] . ' for <b>Batch: </b>' . ' is created successfully');
+//            } else {
+//                echo "else save";
+//                Yii::$app->session->setFlash('red-', '<i class="fa fa-warning"></i> The combination of <b>Fees Category:</b> ' . $_POST['SummaryOnSite']['computer_id'] . ' has already been taken.');
+//            }
+            //endfor;
             echo "<br>";
             //exit();
             return $this->redirect(['view', 'id' => $model->summary_id]);

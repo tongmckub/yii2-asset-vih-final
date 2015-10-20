99f3e727c639b33c58057cab34f14f70329b0e98
warakorn.p <warakorn.p@TONG_IT.srivichai3.local>
2015-10-20 17:31:21 +0700

    - ToggleSoftware = Pass - Calendar = Pass - Header = Modify

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -1,4 +1,5 @@
 <?php
+
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
@@ -6,33 +7,33 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 /* @var $model \common\models\LoginForm */
 
-$this->title = 'Sign In';
+$this->title = 'VIH Asset Manager';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="form-box" id="login-box">
-
-    <div class="header"><?= Html::encode($this->title) ?></div>
+    <div class="header bg-blue"><?= Html::encode($this->title) ?></div>
     <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-    <div class="body bg-gray">
-        <p>Please fill out the following fields to login:</p>
-        <?= $form->field($model, 'username') ?>
-        <?= $form->field($model, 'password')->passwordInput() ?>
+    <div class="body bg-gray" style="margin-top: -20px;">
+        <div class="form-group has-feedback">
+            <?= $form->field($model, 'username')->textInput(['placeholder' => 'Username', 'class' => 'form-control'])->label(false) ?>
+            <span class="glyphicon glyphicon-user form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+            <?= $form->field($model, 'password')->passwordInput(['class' => 'form-control', 'placeholder' => 'Password'])->label(false) ?>
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        </div>
         <?= $form->field($model, 'rememberMe')->checkbox() ?>
     </div>
     <div class="footer">
-
-        <?= Html::submitButton('Login', ['class' => 'btn bg-olive btn-block', 'name' => 'login-button']) ?>
-
-        <p><a href="#">I forgot my password</a></p>
-        
+        <?= Html::submitButton('Login', ['class' => 'btn bg-blue btn-block', 'name' => 'login-button']) ?>      
     </div>
     <?php ActiveForm::end(); ?>
-      <?php if(\Yii::$app->session->hasFlash('loginError')) : ?>
-	    <div class="alert alert-danger alert-dismissible" style="margin-top: 5%;">
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-		<span aria-hidden="true">&times;</span></button>
-		<?php echo \Yii::$app->session->getFlash('loginError'); ?>
-	    </div>
-       <?php endif; ?>
+    <?php if (\Yii::$app->session->hasFlash('loginError')) : ?>
+        <div class="alert alert-danger alert-dismissible" style="margin-top: 5%;">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span></button>
+            <?php echo \Yii::$app->session->getFlash('loginError'); ?>
+        </div>
+    <?php endif; ?>
 </div>
 
